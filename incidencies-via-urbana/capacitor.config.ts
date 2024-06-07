e6499215a2941b1374e77b731320a437f6aee7a8
Pep Pallares <peppallaresreverte@gmail.com>
2024-06-07 18:24:10 +0200

    (feat) Initial commit with blank ionic tabs app

---
+++ incidencies-via-urbana/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'incidencies-via-urbana',
+  webDir: 'www'
+};
+
+export default config;
