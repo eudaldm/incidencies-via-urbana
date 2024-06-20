50f4370b09b518df8954bc7e809a7478baafd8c9
eudaldm <eudaldmanzano@gmail.com>
2024-06-20 17:27:33 +0200

    Project refactored to Standalone (#3)
    (refactor) Project to standalone

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { MarkersService } from '../services/markers/markers.service';
 import { Geolocation } from '@capacitor/geolocation';
 import { Marker } from '@capacitor/google-maps';
+import { NgFor } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss'],
+    standalone: true,
+    imports: [IonicModule, NgFor]
 })
 export class Tab1Page {
 
