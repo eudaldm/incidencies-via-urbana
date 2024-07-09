9aeb099beedf0eea2c6d06254e14c301c548c999
Eudald Manzano <eudaldmanzano@gmail.com>
2024-07-09 21:05:46 +0200

    (fix) Check if the app is running on a mobile device to avoid OneSignal capacitor errors when running on a web browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'
+import { IonApp, IonRouterOutlet, Platform } from '@ionic/angular/standalone'
 import OneSignal from 'onesignal-cordova-plugin';
 
 @Component({
@@ -10,20 +10,23 @@ import OneSignal from 'onesignal-cordova-plugin';
   imports: [IonApp, IonRouterOutlet]
 })
 export class AppComponent {
-  constructor() {
-    // Remove this method to stop OneSignal Debugging
-    OneSignal.Debug.setLogLevel(6)
-    
-    // Replace YOUR_ONESIGNAL_APP_ID with your OneSignal App ID
-    OneSignal.initialize("b0283ed4-860b-4c3b-adc2-69410c7a1c04");
-
-    OneSignal.Notifications.addEventListener('click', async (e) => {
-      let clickData = await e.notification;
-      console.log("Notification Clicked : " + clickData);
-    })
-
-    OneSignal.Notifications.requestPermission(true).then((success: Boolean) => {
-      console.log("Notification permission granted " + success);
-    })
+  constructor(private platform: Platform) {
+    // Check if the app is running on a mobile device to turn on OneSignal push notifications
+    if(this.platform.is('mobile')) {
+      // Remove this method to stop OneSignal Debugging
+      OneSignal.Debug.setLogLevel(6);
+      
+      // Replace YOUR_ONESIGNAL_APP_ID with your OneSignal App ID
+      OneSignal.initialize("b0283ed4-860b-4c3b-adc2-69410c7a1c04");
+  
+      OneSignal.Notifications.addEventListener('click', async (e) => {
+        let clickData = await e.notification;
+        console.log("Notification Clicked : " + clickData);
+      });
+  
+      OneSignal.Notifications.requestPermission(true).then((success: Boolean) => {
+        console.log("Notification permission granted " + success);
+      });
+    }
   }
 }
