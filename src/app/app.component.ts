d514ce62c2dac5a165179b69c0469a56923bc198
Eudald Manzano <eudaldmanzano@gmail.com>
2024-07-09 14:52:54 +0200

    (feat) Implemented OneSignal notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'
+import OneSignal from 'onesignal-cordova-plugin';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,20 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'
   imports: [IonApp, IonRouterOutlet]
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    // Remove this method to stop OneSignal Debugging
+    OneSignal.Debug.setLogLevel(6)
+    
+    // Replace YOUR_ONESIGNAL_APP_ID with your OneSignal App ID
+    OneSignal.initialize("b0283ed4-860b-4c3b-adc2-69410c7a1c04");
+
+    OneSignal.Notifications.addEventListener('click', async (e) => {
+      let clickData = await e.notification;
+      console.log("Notification Clicked : " + clickData);
+    })
+
+    OneSignal.Notifications.requestPermission(true).then((success: Boolean) => {
+      console.log("Notification permission granted " + success);
+    })
+  }
 }
