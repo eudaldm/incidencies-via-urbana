3a3fd83559d117a488b2b8f96fe8806535303c14
Pep Pallarés <peppallaresreverte@gmail.com>
2024-07-11 22:02:37 +0200

    (fix) Remove api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     projectId: "incidenciesviaurbana",
     appId: "1:25019606126:web:ee8e28d36fc5e4dbdf327f",
     storageBucket: "incidenciesviaurbana.appspot.com",
-    apiKey: "AIzaSyDJdFLcCNiXdtBinZP_dgeG4ay47BLsjzU",
+    apiKey: "",
     authDomain: "incidenciesviaurbana.firebaseapp.com",
     messagingSenderId: "25019606126"
   }
